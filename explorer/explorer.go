12d42abfc615da64c79223b37141f0164aa48515
Eric Cobb <ecobb@sonatype.com>
2017-02-09 14:27:18 -0700

    Walking directory

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -1,15 +1,26 @@
 package explore
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 // Exploration to be explored
 type Exploration struct {
 	Path string
 }
 
+func (exploration *Exploration) visit(path string, f os.FileInfo, err error) error {
+	fmt.Printf("Visited: %s\n", path)
+	return nil
+}
+
 // Run executes the exploration
 func (exploration *Exploration) Run() {
-	fmt.Printf("exploring %s", exploration.Path)
+	fmt.Printf("exploring %s \n", exploration.Path)
+	err := filepath.Walk(exploration.Path, exploration.visit)
+	fmt.Printf("filepath.Walk() returned %v\n", err)
 }
 
 // Explore defines the Blob Store directory to explore
